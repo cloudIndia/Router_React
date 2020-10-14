689b901bfcb997f290c46c2db0c871db57461310
System Administrator <root@Badals-MacBook-Pro.local>
2020-10-15 01:26:47 +0530

    React routing first project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Post from './component/post'
+import Profile from './component/profile'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends Component{
+    constructor(props){
+        super(props)
+    }
+    
+    // render
+    render()
+    {
+        return(
+            <div>
+                <h1>Home page</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<BrowserRouter>
+                    <div>
+                    <nav class="navbar navbar-inverse">
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>                        
+                            </button>
+                            <a class="navbar-brand" href="#">Router React</a>
+                            </div>
+                            <div class="collapse navbar-collapse" id="myNavbar">
+                            <ul class="nav navbar-nav">
+                                <li><Link to='/'> HOME </Link></li>
+                                <li><Link to='/post'> Post Pe click </Link></li>
+                                <li><Link to='/profile'> Profile pe </Link></li>
+                            </ul>
+                            </div>
+                        </div>
+                    </nav>
+                    </div>
+                <div>
+                    <Route path='/' component={App} exact></Route>
+                    <Route path='/post' component={Post}></Route>
+                    <Route path='/profile' component={Profile}></Route>
+                </div>
+                </BrowserRouter>,document.getElementById('root'))
